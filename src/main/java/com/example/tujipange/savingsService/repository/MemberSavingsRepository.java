cf738ffa1b78ddb1488baae75637cbabe6db762f
Nzovia <nickiemaundu19@gmail.com>
2023-04-17 22:04:01 +0300

    added both member sharing and savings repos

---
+++ src/main/java/com/example/tujipange/savingsService/repository/MemberSavingsRepository.java
@@ -0,0 +1,8 @@
+package com.example.tujipange.savingsService.repository;
+/**
+@author Nicholas Nzovia
+@On 22/03/2023
+@Contact: itsdevelopernic22@gmail.com
+*/
+public interface MemberSavingsRepository {
+}
