cc4229d62220f9b7ecc8d562a509bbeba4ce4e03
Nzovia <nicholas.maundu@roamtech.com>
2023-06-28 10:27:41 +0300

    updated swagger and read me

--- src/main/java/com/example/tujipange/configs/SwaggerConfiguration.java
+++ src/main/java/com/example/tujipange/configs/SwaggerConfiguration.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 /**
  * @author Nicholas Nzovia
@@ -25,6 +29,25 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        Contact contact = new Contact(
+                "Dev Nic",
+                "https://nzovia.github.io/",
+                "itsdevelopernic22@gmail.com"
+        );
+        return new ApiInfo(
+                "Tujipange Api's",
+                "The Apis development are inspired by desire to build complete Backend for Group Contributions",
+                "version 1.0",
+                "",
+                contact,
+                "All rights reserved for the developer",
+                "",
+                Collections.emptyList()
+        );
     }
 }
---
+++ src/main/resources/README.md
@@ -0,0 +1,3 @@
+## Documentation
+#### The Apis are listed in the swagger below when you execute the App locally
+#### (http://localhost:8081/tujipange/swagger-ui/)
